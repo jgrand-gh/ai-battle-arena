79de15ad498b33bb8936f8f13817929f59797992
Josh Grand <8271819+jgrand-gh@users.noreply.github.com>
2025-07-28 00:10:38 -0500

    final review pass of README.md and a small tweak to gemini_manager.py before submission

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from enum import Enum
 from rich import print
 from rich.text import Text
 
-DELAY_PER_WORD = 0.08 # default: 0.24 is average wpm for teenagers apparently, but feels too slow
+DELAY_PER_WORD = 0.16 # default: 0.24 is average wpm for teenagers apparently, but feels too slow
 
 class ColorList(Enum):
     RED = "red"
